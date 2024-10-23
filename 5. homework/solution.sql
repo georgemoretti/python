4bb30b04b1cdb924df084f8cad85b8032bb017e3
Georgii <75439058+georgemoretti@users.noreply.github.com>
2024-10-23 20:47:29 +0300

    Update solution.sql

--- 5. homework/solution.sql
+++ 5. homework/solution.sql
@@ -16,7 +16,7 @@ where unit_price between 3 and 6;
 
 select min(unit_price) min_price
 from products
-where category_id = 1
+where category_id = 1;
 
 /*
 Задача 3
@@ -36,4 +36,4 @@ select supplier_id, max(unit_price) max_price
 from products
 where supplier_id in (1, 3, 5)
 group by supplier_id
-order by supplier_id 
+order by supplier_id;
